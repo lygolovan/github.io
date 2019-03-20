ba20dd770afabf6e42c084c4a013aca590662334
lygolovan <48047812+lygolovan@users.noreply.github.com>
2019-03-20 17:30:06 +0200

    Add files via upload

---
+++ dz8(10)/script/js.js
@@ -0,0 +1,11 @@
+
+function ff(){
+	document.getElementById('title1').style.color="red";
+	document.getElementById('title1').innerHTML = "ХАРАКТЕРИСТИКА";
+	document.getElementById('title2').style.color="green";
+	document.getElementById('title2').innerHTML = "ФОТОЧКИ";
+	document.body.style.background="#006400";
+	document.getElementById('b1').style.backgroundColor = '#00BFFF';
+	
+	
+	}
